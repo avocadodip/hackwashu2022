f1333468af8414f6145c6a61949d6169704fb7be
cewidiupleek <cewidiupleek@gmail.com>
2022-10-16 08:51:36 -0500

    change

--- src/components/FeedPost.svelte
+++ src/components/FeedPost.svelte
@@ -3,8 +3,10 @@
 
   export let rating;
   export let profilePic;
-  export let likeCount;
-  export let respectCount;
+  export let likeCount = 5;
+  export let respectCount = 5;
+  export let commentLikeCount = 5;
+  export let commentRespectCount = 5;
 	export let commentCount;
   export let postContent = 'skeleton';
 	let arrowOneDirection = false;
@@ -15,20 +17,27 @@
 	let isCommentRespect = false;
 
 	const togglePostHeart = () => {
+		if (isPostHeart == false) likeCount++;
+		if (isPostHeart == true) likeCount--;
 		isPostHeart = !isPostHeart;
-
 	}
-
+	
 	const togglePostRespect = () => {
+		if (isPostRespect == false) respectCount++;
+		if (isPostRespect == true) respectCount--;
 		isPostRespect = !isPostRespect;
 	}
 
 	const toggleCommentHeart = () => {
+		if (isCommentHeart == false) commentLikeCount++;
+		if (isCommentHeart == true) commentLikeCount--;
 		isCommentHeart = !isCommentHeart;
 
 	}
 
 	const toggleCommentRespect = () => {
+		if (isCommentRespect == false) commentRespectCount++;
+		if (isCommentRespect == true) commentRespectCount--;
 		isCommentRespect = !isCommentRespect;
 	}
 
@@ -83,22 +92,31 @@
 			<!-- buttons -->
 			<div class="flex gap-7 mt-3">
 				<div class="flex items-center gap-2">
-					<button>
-						<svg
-							xmlns="http://www.w3.org/2000/svg"
-							fill="none"
-							viewBox="0 0 24 24"
-							stroke-width="2"
-							stroke="currentColor"
-							class="w-5 h-5"
-						>
-							<path
-								stroke-linecap="round"
-								stroke-linejoin="round"
-								d="M21 8.25c0-2.485-2.099-4.5-4.688-4.5-1.935 0-3.597 1.126-4.312 2.733-.715-1.607-2.377-2.733-4.313-2.733C5.1 3.75 3 5.765 3 8.25c0 7.22 9 12 9 12s9-4.78 9-12z"
-							/>
-						</svg>
-					</button>
+					{#if isPostHeart == false}
+						<button on:click={togglePostHeart}>
+							<svg
+								xmlns="http://www.w3.org/2000/svg"
+								fill="none"
+								viewBox="0 0 24 24"
+								stroke-width="2"
+								stroke="currentColor"
+								class="w-5 h-5"
+							>
+								<path
+									stroke-linecap="round"
+									stroke-linejoin="round"
+									d="M21 8.25c0-2.485-2.099-4.5-4.688-4.5-1.935 0-3.597 1.126-4.312 2.733-.715-1.607-2.377-2.733-4.313-2.733C5.1 3.75 3 5.765 3 8.25c0 7.22 9 12 9 12s9-4.78 9-12z"
+								/>
+							</svg>
+						</button>
+						{:else}
+						<button on:click={togglePostHeart}>
+							<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" class="w-5 h-5">
+								<path d="M11.645 20.91l-.007-.003-.022-.012a15.247 15.247 0 01-.383-.218 25.18 25.18 0 01-4.244-3.17C4.688 15.36 2.25 12.174 2.25 8.25 2.25 5.322 4.714 3 7.688 3A5.5 5.5 0 0112 5.052 5.5 5.5 0 0116.313 3c2.973 0 5.437 2.322 5.437 5.25 0 3.925-2.438 7.111-4.739 9.256a25.175 25.175 0 01-4.244 3.17 15.247 15.247 0 01-.383.219l-.022.012-.007.004-.003.001a.752.752 0 01-.704 0l-.003-.001z" />
+							</svg>
+							
+						</button>
+					{/if}
 					<p class="text-sm font-medium pt-1">{likeCount}</p>
 				</div>
 				<div class="flex items-center gap-2">
@@ -213,23 +231,32 @@
 			<!-- buttons -->
 			<div class="flex gap-7 mt-3">
 				<div class="flex items-center gap-2">
-					<button>
-						<svg
-							xmlns="http://www.w3.org/2000/svg"
-							fill="none"
-							viewBox="0 0 24 24"
-							stroke-width="2"
-							stroke="currentColor"
-							class="w-5 h-5"
-						>
-							<path
-								stroke-linecap="round"
-								stroke-linejoin="round"
-								d="M21 8.25c0-2.485-2.099-4.5-4.688-4.5-1.935 0-3.597 1.126-4.312 2.733-.715-1.607-2.377-2.733-4.313-2.733C5.1 3.75 3 5.765 3 8.25c0 7.22 9 12 9 12s9-4.78 9-12z"
-							/>
-						</svg>
-					</button>
-					<p class="text-sm font-medium pt-1">{likeCount}</p>
+					{#if isCommentHeart == false}
+						<button on:click={toggleCommentHeart}>
+							<svg
+								xmlns="http://www.w3.org/2000/svg"
+								fill="none"
+								viewBox="0 0 24 24"
+								stroke-width="2"
+								stroke="currentColor"
+								class="w-5 h-5"
+							>
+								<path
+									stroke-linecap="round"
+									stroke-linejoin="round"
+									d="M21 8.25c0-2.485-2.099-4.5-4.688-4.5-1.935 0-3.597 1.126-4.312 2.733-.715-1.607-2.377-2.733-4.313-2.733C5.1 3.75 3 5.765 3 8.25c0 7.22 9 12 9 12s9-4.78 9-12z"
+								/>
+							</svg>
+						</button>
+						{:else}
+						<button on:click={toggleCommentHeart}>
+							<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" class="w-5 h-5">
+								<path d="M11.645 20.91l-.007-.003-.022-.012a15.247 15.247 0 01-.383-.218 25.18 25.18 0 01-4.244-3.17C4.688 15.36 2.25 12.174 2.25 8.25 2.25 5.322 4.714 3 7.688 3A5.5 5.5 0 0112 5.052 5.5 5.5 0 0116.313 3c2.973 0 5.437 2.322 5.437 5.25 0 3.925-2.438 7.111-4.739 9.256a25.175 25.175 0 01-4.244 3.17 15.247 15.247 0 01-.383.219l-.022.012-.007.004-.003.001a.752.752 0 01-.704 0l-.003-.001z" />
+							</svg>
+							
+						</button>
+					{/if}
+					<p class="text-sm font-medium pt-1">{commentLikeCount}</p>
 				</div>
 				<div class="flex items-center gap-2">
 					{#if isCommentRespect == false}
@@ -268,7 +295,7 @@
 						</svg>
 						</button>
 					{/if}
-					<p class="text-sm font-medium pt-1">{respectCount}</p>
+					<p class="text-sm font-medium pt-1">{commentRespectCount}</p>
 				</div>
 				<!-- COMMENT -->
 				<div class="flex items-center gap-2">
